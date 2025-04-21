177ef283f3b8772f7a2e360f992406d4b6927f0d
DamGarcia <damg883@gmail.com>
2025-04-21 09:30:01 -0400

    Updated Looping Exercises - corrected ForLoop to count backwards from 10

--- LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -4,7 +4,7 @@ public class ForLoop {
 
     public static void main(String[] args) throws InterruptedException{
 
-            for(int i = 1; i < 11; i++){
+            for(int i = 10; i > 0; i--){
                 System.out.println(i);
                 Thread.sleep(1000);
             }
